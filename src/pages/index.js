b15b73e3b185d43cb57ab3c20c1c251719e12c65
Adam Teo <adam.teo321@gmail.com>
2023-03-25 23:58:59 -0500

    move fetch to temp file to prevent conflict

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { useState } from "react";
-import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const [url, setUrl] = useState("");
-  const [bruh, setBruh] = useState("");
-
-  useEffect(() => {
-    console.log(bruh);
-  }, [bruh]);
-
-  const scrape = async (e) => {
-    e.preventDefault();
-    const test = fetch("/api/fetch", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ link: url }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
-    setBruh(test);
-  };
-
-  return (
-    <div>
-      <form onSubmit={scrape}>
-        <input
-          id="url"
-          name="url"
-          value={url}
-          required={true}
-          onChange={(e) => {
-            setUrl(e.target.value);
-          }}
-        />
-      </form>
-    </div>
-  );
+  return <div></div>;
 }
